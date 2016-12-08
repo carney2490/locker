d898509f762589362320f19e6ab7d3d40fcb28fa
heathershockney <shockney@atlanticbb.net>
2016-12-08 16:51:14 -0500

    updated images, personalization charges

--- app.rb
+++ app.rb
@@ -13,12 +13,12 @@ db_params = {
    port:ENV['port'],
    dbname:ENV['dbname'],
    user:ENV['dbuser'],
-   password:ENV['dbpassword'],    
+   password:ENV['dbpassword'],
 }
 
 db = PG::Connection.new(db_params)
 
-set :sessions, 
+set :sessions,
   key: ENV['sessionkey'],
   domain:  ENV['domain'],
   path: '/',
@@ -57,8 +57,8 @@ get '/contact' do
 end
 
 Mail.defaults do
-  delivery_method :smtp, 
-  address: "email-smtp.us-east-1.amazonaws.com", 
+  delivery_method :smtp,
+  address: "email-smtp.us-east-1.amazonaws.com",
   port: 587,
   :user_name  => ENV['a3smtpuser'],
   :password   => ENV['a3smtppass'],
@@ -68,17 +68,17 @@ end
 post '/contact' do
   name = params[:firstname]
   lname= params[:lastname]
-  email= params[:email]                  
+  email= params[:email]
   comments = params[:message]
   subject= params[:subject]
   email_body = erb(:email2,:layout=>false, :locals=>{:subject => subject,:firstname => name, :lastname => lname, :email => email, :message => comments})
-  
+
   mail = Mail.new do
     from         ENV['from']
     to           email
     bcc          ENV['from']
     subject      subject
-    
+
     html_part do
       content_type 'text/html'
       body         email_body
@@ -91,7 +91,7 @@ end
 
 post '/submit' do
     erb :submit1
-end 
+end
 
 get '/customer_order' do
     @title = 'Orders'
@@ -113,29 +113,29 @@ post '/customer_register' do
     email = params[:email]
     phone = params[:phone]
     password = params[:password]
-    
+
     #This is for creating profile and preventing duplication
     check_email = db.exec("SELECT * FROM users WHERE email = '#{email}'")
-    
-    hash = BCrypt::Password.create(password, :cost => 11) 
-    
-    
+
+    hash = BCrypt::Password.create(password, :cost => 11)
+
+
         if check_email.num_tuples.zero? == false
             erb :customer_register, :locals => {:message => " ", :message1 => "That email already exists"}
         else
-             db.exec ("INSERT INTO users (fname, lname, address, city, state, zipcode, email, phone, encrypted_password,name) 
+             db.exec ("INSERT INTO users (fname, lname, address, city, state, zipcode, email, phone, encrypted_password,name)
                       VALUES ('#{fname}', '#{lname}', '#{address}','#{city}', '#{state}', '#{zipcode}', '#{email}','#{phone}', '#{hash}','#{fname} #{lname}')" )
             erb :success, :locals => {:message => "You have successfully registered.", :message1 => " "}
         end
 end
 
 post '/facebook' do
-    @title = 'Facebook Login'    
+    @title = 'Facebook Login'
     name= params[:name]
     email = params[:email]
-    
+
     check_email = db.exec("SELECT * FROM users WHERE email = '#{email}'")
-    
+
     if check_email.count > 0
         puts '#{check_email}'
     else
@@ -143,18 +143,18 @@ post '/facebook' do
     end
     session[:user] = name
     session[:email] = email
-    
-    redirect '/'   
+
+    redirect '/'
 end
 
 post '/google' do
     @title = 'Google Login'
-    
+
     name = params[:gname]
     email = params[:gemail]
-    
+
     check_email = db.exec("SELECT * FROM users WHERE email = '#{email}'")
-    
+
     if check_email.count > 0
         puts '#{check_email}'
     else
@@ -162,8 +162,8 @@ post '/google' do
     end
     session[:user] = name
     session[:email] = email
-    
-    redirect '/'   
+
+    redirect '/'
 end
 
 
@@ -176,26 +176,26 @@ post '/login' do
             error = erb :login, :locals => {:message => "Invalid email and password combination",:user => session[:user]}
             return error
         end
-    
+
     password1 = match_login[0]['encrypted_password']
     comparePassword = BCrypt::Password.new(password1)
-  
+
     user_email = match_login[0]['email']
    name = match_login[0]['name']
     user_type = match_login[0]['user_type']
 
-    
+
       if match_login[0]['email'] == email && comparePassword == password
-          session[:email] = user_email  
+          session[:email] = user_email
           session[:usertype] = user_type
           session[:user] = name
           puts "authenticated"
           erb :index
       else
       erb :login, :locals => {:message => "Invalid username and password combination",:user => session[:user]}
-      end 
+      end
 end
-      
+
 get '/logout' do
   session[:user] = nil
   session[:usertype] = nil
@@ -209,14 +209,14 @@ get '/admin_page' do
     @title = 'Admin Page'
     mailing_list = db.exec("SELECT email FROM mailing_list")
     mailing_list = mailing_list.values.join(", ")
-   
+
     erb :admin_page,:locals =>{:mailing_list => mailing_list}
 end
 
 get '/edit_profile'do
     @title = 'Edit Profile'
     users = db.exec("SELECT * FROM users WHERE email='#{session[:email]}'")
-    edit_profile = db.exec("SELECT fname,lname,address,city,state,zipcode,email,phone,company FROM users WHERE email = '#{session[:email]}' ")  
+    edit_profile = db.exec("SELECT fname,lname,address,city,state,zipcode,email,phone,company FROM users WHERE email = '#{session[:email]}' ")
     current_profile= db.exec("SELECT * FROM users WHERE email='#{session[:email]}'")
     erb :edit_profile, :locals => {:edit_profile => edit_profile,:users =>users,:current_profile =>current_profile}
 end
@@ -231,7 +231,7 @@ post '/edit_profile' do
    email = params[:email]
    phone = params[:phone]
    company = params[:company]
-    users = db.exec("SELECT * FROM users WHERE email='#{session[:email]}'")       
+    users = db.exec("SELECT * FROM users WHERE email='#{session[:email]}'")
     update_profile = db.exec ("UPDATE users SET (fname,lname, address, city, state, zipcode, email, phone,company)  =  ('#{fname}','#{lname}', '#{address}','#{city}', '#{state}', '#{zipcode}', '#{email}', '#{phone}', '#{company}' ) WHERE email = '#{email}'" )
     redirect '/edit_profile'
 end
@@ -248,11 +248,11 @@ end
 
 post '/product_details' do
     url = params[ :url]
-    
+
     size_price = db.exec("SELECT size, price FROM products2 WHERE product_url = '#{url}' ORDER BY size Asc")
-    
+
     product_info = db.exec("SELECT product_name, product_description, order_information, product_url, personalization FROM products2 WHERE product_url = '#{url}' LIMIT 1")
-    
+
 
     erb :product_details, :locals => {:product_info => product_info, :size_price => size_price}
 end
@@ -261,18 +261,18 @@ get '/shop_cart' do
     @title = 'Shopping Cart'
     session[:cart] ? cart = session[:cart] : cart = []
     get_order_total()
-     
+
     erb :shop_cart, :locals => {:cart => cart, :ordertotal => session[:ordertotal], :name => "",:price => "",
                                 :quantity => "",:size => "",:line1 => "",:line2 => "",:line3 => "", :line4 => "", :url => ""}
-   
-  
+
+
 end
 
 post '/add_to_cart' do
     @title = 'Shopping Cart'
-    
+
     session[:cart] ||= []
-        session[:cart] ? cart = session[:cart] : cart = []
+        # session[:cart] ? cart = session[:cart] : cart = []
 
     name = params[:productName]
     description = params[:productDescription]
@@ -280,17 +280,22 @@ post '/add_to_cart' do
     size = params[:size]
     quantity = params[:quantity].to_i
     price = params[:price].to_f
-    line1 = params[:line1]
-    line2 = params[:line2]
-    line3 = params[:line3]
-    line4 = params[:line4]
+    line1 = params[:youth_name] || ""
+    line2 = params[:youth_number] || ""
+    line3 = params[:adult_name] || ""
+    line4 = params[:adult_number] || ""
+    if line1.length > 0 || line3.length > 0
+      price += 3.00
+    end
+    if line2.length > 0 || line4.length > 0
+      price += 2.00
+    end
     total = quantity * price
-        
     session[:cart].push({"productname" => name, "description" => description, "url" => url, "size" => size,
                          "quantity" => quantity, "price" => price,"total" => total, "line1" => line1,
                          "line2" => line2, "line3" => line3, "line4" => line4})
-
-    erb :shop_cart, :locals => {:cart => cart, :ordertotal => session[:ordertotal], :name => name ,:price => price,
+    fee = session[:cart].length * 0.75
+    erb :shop_cart, :locals => {:cart => session[:cart], :fee => fee, :name => name ,:price => price,
                                 :quantity => quantity,:size => size,:line1 => line1,:line2 => line2,:line3 => line3,
                                 :line4 => line4, :url => url}
 end
@@ -352,8 +357,8 @@ end
 post '/checkout1' do
     @title = 'Checkout Step1'
     session[:customerinfo] ||= []
-   
-    
+
+
    erb :checkout1, :locals => {:cart => session[:cart],:ordertotal => session[:ordertotal],:customerinfo => customerinfo}
 
 end
@@ -370,7 +375,7 @@ post '/checkout2' do
     email = params[:email]
      session[:cart2] = []
      session[:cart2].push({"firstname" => firstname, "lastname" => lastname, "company" => company, "street" => street, "city" => city , "state" => state, "zip" => zip, "phone" => phone, "email" => email})
-     
+
 
     erb :checkout2,:locals => {:cart => session[:cart],:customerinfo => session[:customerinfo],:ordertotal => session[:ordertotal],:cart2 => session[:cart2]}
   end
@@ -389,13 +394,13 @@ post '/checkout4' do
     order_number = '2'
     delivery_method = params[:delivery_method]
 
-    db.exec("INSERT INTO users(fname,lname,address,city,state,zipcode) 
+    db.exec("INSERT INTO users(fname,lname,address,city,state,zipcode)
             VALUES ('#{firstname}','#{lastname}','#{street}','#{city}','#{state}','#{zip}')")
-       
+
       session[:cart].each do |m|
 
-     
-          db.exec ("INSERT INTO orders (product_name,quantity,line1,line2,line3,line4,unit_price,total_price,order_date,delivery_method,payment_method) 
+
+          db.exec ("INSERT INTO orders (product_name,quantity,line1,line2,line3,line4,unit_price,total_price,order_date,delivery_method,payment_method)
                     VALUES ('#{m['productname']}','#{m['quantity']}','#{m['line1']}','#{m['line2']}','#{m['line3']}','#{m['line4']}',
                     '#{m['price']}','#{m['total']}','#{order_date}','#{delivery_method}','#{payment_method}')" )
       end
@@ -404,9 +409,9 @@ end
 
 get '/receipt' do
     @title = 'Receipt'
-   
+
     erb :receipt,:locals => {:cart => session[:cart],:cart2 => session[:cart2],:ordertotal => session[:ordertotal],:message =>"Thanks for your order, here is a receipt you can print for your records."}
-  
+
   redirect to ('/paypal')
 end
 get '/category_shirts' do
@@ -431,7 +436,7 @@ end
 post '/subscribe' do
     email= params[:email]
     check_email = db.exec("SELECT * FROM mailing_list WHERE email = '#{email}'")
-       
+
     if
         check_email.num_tuples.zero? == false
             erb :mailing_list, :locals => {:message => "You have already joined our mailing list"}
@@ -449,38 +454,38 @@ get '/send_emails' do
    subscribers = db.exec("select email from mailing_list")
 
   erb :subscribers, :locals => {:subscribers => subscribers, :message => ""}
-end 
+end
 
 post '/send_mail_to_list' do
       subject = params[:subject]
       subscribers = db.exec("select email from mailing_list")
       message = params[:message]
-      
+
 
       subscribers.each do |email|
       emails = email["email"]
-  
+
     Pony.mail(
         :to => "#{emails}",
         :from => 'info@minedminds.org',
-        :subject => "#{subject}", 
-        :content_type => 'text/html', 
+        :subject => "#{subject}",
+        :content_type => 'text/html',
         :body => erb(:send_mailer,:layout=>false,:locals=>{:subject => subject,:message => message}),
-        :via => :smtp, 
+        :via => :smtp,
         :via_options => {
           :address              => 'smtp.gmail.com',
           :port                 => '587',
           :enable_starttls_auto => true,
            :user_name           => ENV['email'],
            :password            => ENV['email_pass'],
-           :authentication       => :plain, 
-           :domain               => "minedminds-mailinglist.herokuapp.com" 
+           :authentication       => :plain,
+           :domain               => "minedminds-mailinglist.herokuapp.com"
         }
       )
 end
    erb :subscribers, :locals => {:subscribers => "",:message => "You Sent an email to your list"}
-end 
+end
 
-get '/margaretbellmiller' do
-    erb :margaretbellmiller
+get '/wchsgirlsbasketball' do
+    erb :wchsgirlsbasketball
 end
