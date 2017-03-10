1a8693d0b8c949c70898ca1254ba4e424dc87237
heathershockney <shockney@atlanticbb.net>
2017-03-10 12:54:23 -0500

    adding new campaign

--- public/js/app.js
+++ public/js/app.js
@@ -2,7 +2,7 @@
 //$('#school').change(function(){
 //  var selectionImage = $('.selection img');
 //  var itemType = $('#school').val();
-//  
+//
 //  if (itemType === "casd") {
 //    selectionImage.prop("src","img/school_logos/mightymikelogo.jpg");
 //  } else if (itemType === "wcsd") {
@@ -13,16 +13,19 @@
 //    selectionImage.prop("src","img/school_logos/jefferson_sm.jpg");
 //  } else if (itemType === "sgsd") {
 //    selectionImage.prop("src","img/school_logos/maple_leaf_sm.jpg");
-//  } else if (itemType === "wu") {                  
+//  } else if (itemType === "wu") {
 //    selectionImage.prop("src","img/school_logos/WaynesburgJackets_sm.jpg");
-//  } 
+//  }
 //});
 
 // change product price when clicking on size
+
 $('#productPage label').click(function(){
   var price = $('[name="size"]:checked').attr('data-price');
-  $('#price').attr("value",price);
+  $('#price').attr("value", price);
 });
+
+
 
 $('.fa-trash-o').click(function(){
   var index = $(this).attr("data-index");
@@ -48,7 +51,7 @@ $(document).ready(function(){
             }, 800);
             return false;
         });
-        
+
         $('#back-to-top').tooltip('show');
 
 });
